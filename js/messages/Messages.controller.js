36525e057e203156157949a3ebd07f86725c620a
Chase (Ryan Cline) <ryancline88@yahoo.com>
2017-11-29 16:45:07 -0700

    Updating starred data in database

--- js/messages/Messages.controller.js
+++ js/messages/Messages.controller.js
@@ -8,5 +8,21 @@
     .then((response) => {
       vm.mail = response.data['_embedded'].messages
     })
+
+    vm.updateStarred = (id, bool) => {
+      let data = {
+        "messageIds": [id],
+        "command": "star",
+        "star": bool
+      }
+      $http.patch('https://ng-inbox-server.herokuapp.com/api/messages', data)
+      .then(() => {
+        console.log('finished put request');
+        $http.get('https://ng-inbox-server.herokuapp.com/api/messages')
+        .then((response) => {
+          vm.mail = response.data['_embedded'].messages
+        })
+      })
+    }
   }
 })()
